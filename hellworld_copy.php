52387a7a2cf70694ea8c5535d6567cc7b664f024
Mizuki Kato <kato@sindbad-int.com>
2013-11-13 22:47:48 +0900

    the new

--- hellworld_copy.php
+++ hellworld_copy.php
@@ -1,5 +1,194 @@
-<?php
-echo Hello-World
-sindbadint.com
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml" xml:lang="ja" lang="ja">
+<head>
+<meta http-equiv="Content-Language" content="ja" />
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+<meta http-equiv="Content-Style-Type" content="text/css" />
+<meta http-equiv="Content-Script-Type" content="text/javascript" />
+<title>数学ミラクルマスター｜中学生6000人を伸ばした数学教材</title>
+<meta name="author" content="Ai教研" />
+<meta name="keywords" content="数学、中学数学、高校受験数学、数学教材、数学問題集、数学苦手" />
+<meta name="description" content="全国100校以上の公立中学・学習塾でも導入！東大卒のカリスマ講師・数学の超プロが作った数学教材。高校受験・高校入試で1～2ランク上を目指せる、数学が苦手な中学生のための教材です。" />
+<?php include_once($_SERVER['DOCUMENT_ROOT']."/inc/head.php"); ?>
+</head>
 
-?>
+<body id="home">
+<div id="container">
+
+	<!-- ヘッダー -->
+	<div id="header">
+		<?php include_once($_SERVER['DOCUMENT_ROOT']."/inc/header_test.php"); ?>
+	<!-- ヘッダー --></div>
+
+	<!-- グローバルナビ -->
+		<?php include_once($_SERVER['DOCUMENT_ROOT']."/inc/gnav.php"); ?>
+	<!-- グローバルナビ -->
+
+	<!-- コンテンツ -->
+	<div id="content">
+		<div id="content-inner-home" class="cf">
+
+			<div class="main-visual">
+				<div class="top_cart_btn">
+					<a href="/about/" title="詳細はこちら"><img src="/img/top/top_cart_btn.jpg" alt="詳細はこちら" /></a>
+				</div>
+				<div class="catch">
+					<img src="/img/top/main_visual.jpg" alt="東大卒・数学のプロ講師が開発！中学生6000名以上を伸ばした教材" />
+				</div>
+				<div class="voice">
+					<img src="/img/top/img_voice.jpg" alt="実際に成績が上がった生徒の喜びの声" />
+				</div>
+			</div>
+
+			<!-- メイン -->
+			<div id="main">
+				<div class="bnr-about">
+					<a  href="/about/" title="詳しい使い方はこちら"><img src="/img/top/btn_about.jpg" alt="中学生6000名以上が伸びた！公立中学校も導入決定！" /></a>
+				</div>
+				<div class="intro">
+					<h2>「数学が苦手な中学生」をお持ちの方へ</h2>
+					<p>
+						 この度は『数学ミラクルマスター』公式サイトにお越し頂きありがとうございます。<br />
+						 数学ミラクルマスターでは、東大卒のカリスマ講師・浜田一志が開発した中学数学の自宅学習教材です。<br />
+						 数学が苦手なお子さんでも、定期テストで20点～60点アップを実現することができます。
+					</p>
+					<p>
+						数学ができないのは、お子さんに数学のセンスや才能がないからではありません。<br />
+						<span class="bold uline">数学はやり方次第で誰でも点数が取れるようになる教科です。</span><br />
+						すぐには信じられないかもしれません。ですが、この『数学ミラクルマスター』を利用することで、すでに<span class="bold uline">全国で6000名以上の中学生が数学の成績を上げているのです。</span>
+					</p>
+					<p>
+						また、『数学ミラクルマスター』は、ご家庭だけでなく、全国100校以上の公立中学、私立中学、学習塾でも利用され、テレビ・マスコミ等でも多数取り上げられています。「数学でお困りの方」「数学の成績を上げたい方」「塾や家庭教師でも数学が上がらなかった方」はぜひご利用をご検討ください。
+					</p>
+					<p>
+						<a target="_blank" href="/about/" title="数学ミラクルマスターのさらに詳しく知りたい方はこちら">&gt;&gt;&nbsp;数学ミラクルマスターのさらに詳しい使い方を知りたい方はこちら</a>
+					</p>
+				</div>
+
+				<div class="news">
+					<h3><img src="/img/top/h3_news.gif" alt="最新情報" /></h3>
+					<div class="news-frame">
+						<ul>
+							<li class="gray">
+								<span class="title">2013/08/10&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;【夏季休暇のお知らせ】</span><br />8月13日(火)から8月16日(金)までお休みをいただきます。<br />期間中にお問い合わせを頂いた場合、8月17日(土)以降にご対応させていただきます。<br />また、商品の発送は、8月19日(月)以降順次発送させていただきます。宜しくお願い致します。
+							</li>
+							<li class="white">
+								<span class="title">2012/10/10&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;【年末年始のお知らせ】</span><br />2012年12月28日(金)から2013年1月4日(金)までお休みを頂きます。<br />期間中にお問い合わせを頂いた場合、2013年1月5日(土)以降にご対応させて頂きます。<br />宜しくお願い致します。
+							</li>
+							<li class="gray">
+								<span class="title">2012/11/27&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;数学ミラクルマスター開発者の浜田一志が、母校・東京大学野球部の監督に就任いたしました。</span>
+
+							</li>
+							<li class="white">
+								<span class="title">2012/10/11&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;10/17（水）&nbsp;浜田の新刊『中学受験 算数の文章題 解法パターンまる覚え100』が発売されます</span>
+
+							</li>
+
+							<li class="gray">
+								<span class="title">2012/10/10&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;町田市立鶴川中学校（東京都）で講演会を開催しました</span><br />
+								浜田が、町田市立鶴川中学校（東京都）のPTA会で講演会を開催しました。<br />
+								「中学生の効果的な勉強法」、「部活と両立して結果を出すコツ」、「モチベーションを上げる方法」など、中学生の保護者の方のお悩みを少しでも解決できる内容をお話ししました。
+							</li>
+
+							<li class="white">
+								<span class="title">2012/4/19&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;数学ミラクルマスター公式サイトをオープンいたしました。</span>
+							</li>
+							<!-- <li class="gray">
+								2012/4/20&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;数学ミラクルマスター公式サイトをオープンいたしました。
+							</li>
+							<li class="white">
+								2012/4/20&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;数学ミラクルマスター公式サイトをオープンいたしました。
+							</li> -->
+						</ul>
+					</div>
+				</div>
+
+				<div class="small-btn" onclick="_gaq.push(['_trackEvent','click btn','footer']);">
+					<div id="flashContent">
+					<object classid="clsid:d27cdb6e-ae6d-11cf-96b8-444553540000" width="600" height="180" id="/inc/btn_order_small" align="middle">
+					<param name="movie" value="/inc/btn_order_small.swf" />
+					<param name="quality" value="best" />
+					<param name="bgcolor" value="#ffffff" />
+					<param name="play" value="true" />
+					<param name="loop" value="true" />
+					<param name="wmode" value="transparent" />
+					<param name="scale" value="showall" />
+					<param name="menu" value="true" />
+					<param name="devicefont" value="false" />
+					<param name="salign" value="" />
+					<param name="allowScriptAccess" value="sameDomain" />
+					<!--[if !IE]>-->
+					<object type="application/x-shockwave-flash" data="/inc/btn_order_small.swf" width="600" height="180">
+					<param name="movie" value="/inc/btn_order_small.swf" />
+					<param name="quality" value="best" />
+					<param name="bgcolor" value="#ffffff" />
+					<param name="play" value="true" />
+					<param name="loop" value="true" />
+					<param name="wmode" value="transparent" />
+					<param name="scale" value="showall" />
+					<param name="menu" value="true" />
+					<param name="devicefont" value="false" />
+					<param name="salign" value="" />
+					<param name="allowScriptAccess" value="sameDomain" />
+					<!--<![endif]-->
+					<a href="http://www.adobe.com/go/getflash">
+					<img src="http://www.adobe.com/images/shared/download_buttons/get_flash_player.gif" alt="Adobe Flash Player を取得" />
+					</a>
+					<!--[if !IE]>-->
+					</object>
+					<!--<![endif]-->
+					</object>
+					</div>
+				</div>
+
+				<div id="bottom-prefecture">
+					<a href="/prefecture/">>>都道府県別公立高校入試情報はこちら</a>
+				</div>
+
+
+
+
+			</div>
+			<!-- メイン -->
+
+			<!-- サブ -->
+		<?php include_once($_SERVER['DOCUMENT_ROOT']."/inc/sub.php"); ?>
+			<!-- サブ -->
+
+
+		</div>
+
+	<!-- コンテンツ --></div>
+
+	<!-- フッター -->
+		<?php include_once($_SERVER['DOCUMENT_ROOT']."/inc/footer.php"); ?>
+	<!-- フッター --></div>
+
+<?php include_once($_SERVER['DOCUMENT_ROOT']."/inc/clickheat.php"); ?>
+<!-- Google Code for 2011&#24180;9&#26376;&#26032;&#36009;&#22770;&#12469;&#12452;&#12488;&#35370;&#21839; Remarketing List -->
+<script type="text/javascript">
+/* <![CDATA[ */
+var google_conversion_id = 1036360806;
+var google_conversion_language = "en";
+var google_conversion_format = "3";
+var google_conversion_color = "666666";
+var google_conversion_label = "riakCJr3kgIQ5riW7gM";
+var google_conversion_value = 0;
+/* ]]> */
+</script>
+<script type="text/javascript" src="http://www.googleadservices.com/pagead/conversion.js">
+</script>
+<noscript>
+<div style="display:inline;">
+<img height="1" width="1" style="border-style:none;" alt="" src="http://www.googleadservices.com/pagead/conversion/1036360806/?label=riakCJr3kgIQ5riW7gM&amp;guid=ON&amp;script=0"/>
+</div>
+</noscript>
+<!-- yahoo_retargeting -->
+<script type="text/javascript" language="javascript">
+var yahoo_retargeting_id = 'PHNV3WQQOK';
+var yahoo_retargeting_label = '';
+</script>
+<script type="text/javascript" language="javascript" src="//b92.yahoo.co.jp/js/s_retargeting.js"></script>
+<!-- /yahoo_retargeting -->
+</body>
+</html>
